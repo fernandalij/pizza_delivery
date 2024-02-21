8168193ac9f4c5d3307298a7058326ecbeb8d9ef
Fernanda Jordão <fernanda.lij@gmail.com>
2024-02-21 20:07:12 -0300

    creating modal events -- bug at addEventListener func

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 //this anonymous function avoids creating many 'document.querySelector' to select the html elements
 const c = (el) => document.querySelector(el)
 const cs = (el) => document.querySelectorAll(el)
+
+let modalQt = 1
+
+
 //mapping all the pizzas to show in the html
 pizzaJson.map((item, index)=>{
     let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true)
@@ -13,21 +17,52 @@ pizzaJson.map((item, index)=>{
     
     pizzaItem.querySelector('a').addEventListener('click', (e) =>{
         e.preventDefault()
+        c('.pizzaWindowArea').style.opacity = 0
+        c('.pizzaWindowArea').style.display = 'flex'
+        setTimeout(() => {
+            c('.pizzaWindowArea').style.opacity = 1
+        }, 200)
+        modalQt = 1
         let pizzaInfo = document.querySelector('.pizzaWindowArea').cloneNode(true)
         c('.pizzaWindowArea').replaceWith(pizzaInfo)
-        document.querySelector('.pizzaWindowArea').style.display = 'flex'
         pizzaInfo.querySelector('.pizzaInfo--desc').innerHTML =`${item.description}`
         pizzaInfo.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${item.price.toFixed(2)}`
         pizzaInfo.querySelector('.pizzaBig img').src =`${item.img}`
         pizzaInfo.querySelector('h1').innerHTML = `${item.name}`
+        c('.pizzaInfo--size.selected').classList.remove("selected")
+        c('.pizzaInfo--qt').innerHTML = modalQt
+        cs('.pizzaInfo--size').forEach((size, sizeIndex) => {
+            if(sizeIndex === 2){
+                size.classList.add("selected")
+            }
+            size.querySelector('span').innerHTML = item.sizes[sizeIndex]
+        })
     })
-    
     c('.pizza-area').append(pizzaItem)
-    
-
-
 })
 
+//MODAL FUNCTIONS
+function closeModal(){
+    c('.pizzaWindowArea').style.opacity = 0
+    setTimeout(() => {
+        c('.pizzaWindowArea').style.display = 'none'
+    }, 500)
+}
 
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+    item.addEventListener('click', closeModal)
+})
 
+c('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
+    console.log("clicou")
+    if(modalQt > 1){
+        modalQt--
+        c('.pizzaInfo--qt').innerHTML = modalQt
+    }
+})
 
+c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
+    console.log("clicou")
+    modalQt++
+    c('.pizzaInfo--qt').innerHTML = modalQt
+})
